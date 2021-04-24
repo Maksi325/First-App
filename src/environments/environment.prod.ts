18abe5ce88798b03a4b0a2b67cd0753cfc868344
Maksim Majmari <Maksi325>
2021-04-24 17:02:06 +0200

    Configuring Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAmBTQ_XaPOe5OMlGW2OSj-1ZYG5U4Plpg",
+    authDomain: "online-shop-800f4.firebaseapp.com",
+    projectId: "online-shop-800f4",
+    storageBucket: "online-shop-800f4.appspot.com",
+    messagingSenderId: "1056046847489",
+    appId: "1:1056046847489:web:19e81e499823b1ca605200",
+    measurementId: "G-GB156TZHFV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAmBTQ_XaPOe5OMlGW2OSj-1ZYG5U4Plpg",
+    authDomain: "online-shop-800f4.firebaseapp.com",
+    projectId: "online-shop-800f4",
+    storageBucket: "online-shop-800f4.appspot.com",
+    messagingSenderId: "1056046847489",
+    appId: "1:1056046847489:web:19e81e499823b1ca605200",
+    measurementId: "G-GB156TZHFV"
+  }
 };
 
 /*
